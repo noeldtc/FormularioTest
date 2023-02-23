c8e3770efa93afd250ea200f25949cfd2f2277ec
ddeltejo <daniel-noel.del-tejo-catala.external@capgemini.com>
2023-02-23 10:32:40 +0100

    cambios

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function myFunction() {
   let lastNameVal = document.getElementById("lname").value;
   let message = "Este es el nombre " +nameVal + " " +  lastNameVal;
   
-  let lexOrigin;  
+  let lexOrigin = "https://noeldtc.github.io";  
 
 
   
@@ -15,15 +15,17 @@ function myFunction() {
   //Se envia un mensaje a la ventana padre con el mensaje y 
   // el origen
   console.log("Envia mensaje de hijo a padre")
-  window.parent.postMessage(message, "*");
+  window.parent.postMessage(message, lexOrigin);
   
   //Se asigna que si llega un mensaje con "message" la funcion de respuesta es handleMessage
   window.addEventListener("message", handle);
 }
 
+////AQUI SE ENCUENTRA EL PROBLEMA //
 function handle(event){
   console.log('Origin '+ event.origin);
   console.log('data '+ event.data);
   lexOrigin =  event.data;
 
 }
+////
